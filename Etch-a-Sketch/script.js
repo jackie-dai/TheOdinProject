8f2d66f4e639886695261018c09d8c8b09ff28b0
kutekirisaki <jackie123768@gmail.com>
2023-05-23 20:22:59 -0700

    Rough finish of Etch-a-Sketch

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,35 +1,67 @@
 const grid = document.getElementById("grid");
 const gridSize = document.getElementById("gridSizeSlider");
 const sliderLabel = document.getElementById("gridSizeLabel");
+const canvasSize = 540;
+/* BUTTONS */
+const clearBtn = document.getElementById("clearBtn");
 
-gridSize.addEventListener("input", updateSliderLabel);
+let isDrawing = false;
 
-function updateSliderLabel(e) {
-    sliderLabel.innerHTML = e.target.value + " x " + e.target.value;
-    console.log(960 / e.target.value)
+gridSize.addEventListener("input", changeGrid);
+clearBtn.addEventListener("click", function() {
+    clearChildren(grid);
+    makeGrid(gridSize.value)
+});
+
+function changeGrid(e) {
+    updateSliderLabel(e.target.value);
+    clearChildren(grid);
+    makeGrid(e.target.value);
+}
+
+function updateSliderLabel(size) {
+    sliderLabel.innerHTML = size + " x " + size;
 }
 
 
-
 function makeGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         let newSquare = document.createElement("div");
         newSquare.className = "square";
-        newSquare.width = 960/gridSize
-        newSquare.height = 960/gridSize
+        newSquare.style.width = canvasSize / gridSize + "px";
+        newSquare.style.height = canvasSize / gridSize + "px";
+        addEventListener("mouseover", hover);
         grid.appendChild(newSquare);
     }
 }
 
 function clearChildren(parent) {
+    console.log("cleasr");
     while (parent.hasChildNodes()) {
         parent.removeChild(parent.firstChild);
     }
 }
 
-function main() {
-    makeGrid();
+addEventListener("mousedown", draw);
+addEventListener("mouseup", turnOffDrawing);
+
+function hover(e) {
+    if (isDrawing) {
+        e.target.style.backgroundColor = "black";
+    }
 }
 
-console.log("sadfads");
+function draw() {
+    isDrawing = true;
+}
+
+function turnOffDrawing() {
+    isDrawing = false;
+}
+
+function main() {
+    makeGrid(gridSize.value);
+}
+
+
 main();
